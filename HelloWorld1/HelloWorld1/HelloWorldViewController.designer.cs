092eb2aa0efcdacb3429510b1749956c78c539f2
svetalexxx <tariqkhan@Tariqs-MacBook-Pro.local>
2014-07-09 11:13:10 -0400

    mac design
    buttons

--- HelloWorld1/HelloWorld1/HelloWorldViewController.designer.cs
+++ HelloWorld1/HelloWorld1/HelloWorldViewController.designer.cs
@@ -1,18 +1,42 @@
 ﻿// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace HelloWorld1
 {
 	[Register ("HelloWorldViewController")]
 	partial class HelloWorldViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton buttonNext { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton buttonPrev { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField labelTitle { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (buttonPrev != null) {
+				buttonPrev.Dispose ();
+				buttonPrev = null;
+			}
+
+			if (buttonNext != null) {
+				buttonNext.Dispose ();
+				buttonNext = null;
+			}
+
+			if (labelTitle != null) {
+				labelTitle.Dispose ();
+				labelTitle = null;
+			}
 		}
 	}
 }
