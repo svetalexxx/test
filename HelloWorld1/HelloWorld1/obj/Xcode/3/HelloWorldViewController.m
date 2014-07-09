c1f4d122e8d51dd32af417dd0bda19bc82b60834
svetalexxx <tariqkhan@Tariqs-MacBook-Pro.local>
2014-07-09 12:30:39 -0400

    fix device

---
+++ HelloWorld1/HelloWorld1/obj/Xcode/3/HelloWorldViewController.m
@@ -0,0 +1,18 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import "HelloWorldViewController.h"
+
+@implementation HelloWorldViewController
+
+@synthesize buttonNext = _buttonNext;
+@synthesize buttonPrev = _buttonPrev;
+@synthesize imageHW = _imageHW;
+@synthesize labelTitle = _labelTitle;
+@synthesize txtDescription = _txtDescription;
+
+@end
